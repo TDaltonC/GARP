7f7f7cc901803fa92d4c8d827d3bc2fb56b422d7
Dalton <tdaltoncombs@gmail.com>
2013-01-17 17:10:45 -0800

    after first pre-pilot

--- PrePilot/RunGARPSubject.m
+++ PrePilot/RunGARPSubject.m
@@ -73,27 +73,27 @@ if item1c == 3; item1 = imread('milk.jpg');   end
 if item1c == 4; item1 = imread('ojuice.jpg'); end
 if item1c == 5; item1 = imread('4juice.jpg'); end
 
-if item2c == 1; item2 = imread('pistach.jpg');   end
-if item2c == 2; item2 = imread('hershekiss.jpg');end
-if item2c == 3; item2 = imread('doritos.jpg');   end
-if item2c == 4; item2 = imread('cheese.jpg');    end
-if item2c == 5; item2 = imread('chips.jpg');     end
-if item2c == 6; item2 = imread('lemonheads.jpg');end
-if item2c == 7; item2 = imread('pretzel.jpg');   end
-if item2c == 8; item2 = imread('oreo.jpg');      end
-if item2c == 9; item2 = imread('peanuts.jpg');   end
-if item2c == 10;item2 = imread('cheezit.jpg');   end
+if item2c == 1; item2 = imread('pistach.jpg');    end
+if item2c == 2; item2 = imread('hersheykiss.jpg');end
+if item2c == 3; item2 = imread('doritos.jpg');    end
+if item2c == 4; item2 = imread('cheese.jpg');     end
+if item2c == 5; item2 = imread('chips.jpg');      end
+if item2c == 6; item2 = imread('lemonheads.jpg'); end
+if item2c == 7; item2 = imread('pretzel.jpg');    end
+if item2c == 8; item2 = imread('oreo.jpg');       end
+if item2c == 9; item2 = imread('peanuts.jpg');    end
+if item2c == 10;item2 = imread('cheezit.jpg');    end
 
-if item3c == 1; item3 = imread('pistach.jpg');   end
-if item3c == 2; item3 = imread('hershekiss.jpg');end
-if item3c == 3; item3 = imread('doritos.jpg');   end
-if item3c == 4; item3 = imread('cheese.jpg');    end
-if item3c == 5; item3 = imread('chips.jpg');     end
-if item3c == 6; item3 = imread('lemonheads.jpg');end
-if item3c == 7; item3 = imread('pretzel.jpg');   end
-if item3c == 8; item3 = imread('oreo.jpg');      end
-if item3c == 9; item3 = imread('peanuts.jpg');   end
-if item3c == 10;item3 = imread('cheezit.jpg');   end
+if item3c == 1; item3 = imread('pistach.jpg');    end
+if item3c == 2; item3 = imread('hersheykiss.jpg');end
+if item3c == 3; item3 = imread('doritos.jpg');    end
+if item3c == 4; item3 = imread('cheese.jpg');     end
+if item3c == 5; item3 = imread('chips.jpg');      end
+if item3c == 6; item3 = imread('lemonheads.jpg'); end
+if item3c == 7; item3 = imread('pretzel.jpg');    end
+if item3c == 8; item3 = imread('oreo.jpg');       end
+if item3c == 9; item3 = imread('peanuts.jpg');    end
+if item3c == 10;item3 = imread('cheezit.jpg');    end
 
 %% Set up the screen
 
@@ -138,10 +138,10 @@ save (recordname, 'settings')
 % Restrict the keys that can be used for the Kb commands [ALL KEYS ARE
 % ENABLED AFTER A cear all command]
 if (ismac)
-            RestrictKeysForKbCheck([9, 13, 41]) %These are the key codes for f, j, and ESCSAPE respectively
+            RestrictKeysForKbCheck([9, 13, 41]) %These are the Mac key codes for f, j, and ESCSAPE respectively
             escKey = KbName('ESCAPE');
         else
-            RestrictKeysForKbCheck(['f', 'j', 'Esc'])
+            RestrictKeysForKbCheck([70, 74, 81]) %THese are the PC keys for f, j and q
             escKey = KbName('Esc');
 end
  
@@ -172,6 +172,12 @@ while i <= long;
     
     
     if trialOrder(i) == 1; %is for the one item (per side) (different items) GARP
+                       left.Item = ['item1';'item2'];
+               left.Amount = [twoItemGARPTasks(1,1,twoItemGARPOrder(twoItemGARPIndex)), ...
+                              twoItemGARPTasks(1,2,twoItemGARPOrder(twoItemGARPIndex))];
+               right.Item = ['item1';'item2'];
+               right.Amount =[twoItemGARPTasks(2,1,twoItemGARPOrder(twoItemGARPIndex)), ...
+                              twoItemGARPTasks(2,2,twoItemGARPOrder(twoItemGARPIndex))]; 
         twoItemGARP(item1, item1, ...
                     item2, item2,...
                     limitGARPTasks(limitGARPOrder(limitGARPIndex),1), 0, ...
@@ -263,44 +269,44 @@ end
     
     
     if trialOrder(i) == 2; %is for the two item (per side) GARP
-               left.Item = ['item1';'item2'];
-               left.Amount = [twoItemGARPTasks(1,1,twoItemGARPOrder(twoItemGARPIndex)), ...
+               left.Item = [item1c,item2c];
+               left.Amount= [twoItemGARPTasks(1,1,twoItemGARPOrder(twoItemGARPIndex)), ...
                               twoItemGARPTasks(1,2,twoItemGARPOrder(twoItemGARPIndex))];
-               right.Item = ['item1';'item2'];
+               right.Item =[item1c,item2c];
                right.Amount =[twoItemGARPTasks(2,1,twoItemGARPOrder(twoItemGARPIndex)), ...
                               twoItemGARPTasks(2,2,twoItemGARPOrder(twoItemGARPIndex))];       
     end
     
     if trialOrder(i) == 3; %is for the three item GARP 1-2 x 1-3
-               left.Item = ['item1';'item2'];
+               left.Item = [item1c,item2];
                left.Amount = [threeItemGARPTasks1213(1,1,threeItemGARPOrder1213(threeItemGARPIndex1213)), ...
                               threeItemGARPTasks1213(1,2,threeItemGARPOrder1213(threeItemGARPIndex1213))];
-               right.Item = ['item1';'item3'];
+               right.Item= [item1c,item3c];
                right.Amount =[threeItemGARPTasks1213(2,1,threeItemGARPOrder1213(threeItemGARPIndex1213)), ...
                               threeItemGARPTasks1213(2,2,threeItemGARPOrder1213(threeItemGARPIndex1213))];
     end
     
     if trialOrder(i) == 4; %is for the three item GARP 1-2 x 2-3
-               left.Item = ['item1';'item2'];
+               left.Item = [item1c,item2c];
                left.Amount = [threeItemGARPTasks1223(1,1,threeItemGARPOrder1223(threeItemGARPIndex1223)), ...
                               threeItemGARPTasks1223(1,2,threeItemGARPOrder1223(threeItemGARPIndex1223))];
-               right.Item = ['item2';'item3'];
+               right.Item =[item2c,item3c];
                right.Amount =[threeItemGARPTasks1223(2,1,threeItemGARPOrder1223(threeItemGARPIndex1223)), ...
                               threeItemGARPTasks1223(2,2,threeItemGARPOrder1223(threeItemGARPIndex1223))];
     end
     
     if trialOrder(i) == 5; %is for the three item GARP 1-3 x 2-3
-               left.Item = ['item1';'item3'];
+               left.Item = [item1c,item3c];
                left.Amount = [threeItemGARPTasks1323(1,1,threeItemGARPOrder1323(threeItemGARPIndex1323)), ...
                               threeItemGARPTasks1323(1,2,threeItemGARPOrder1323(threeItemGARPIndex1323))];
-               right.Item = ['item2';'item3'];
+               right.Item =[item2c,item3c'];
                right.Amount =[threeItemGARPTasks1323(2,1,threeItemGARPOrder1323(threeItemGARPIndex1323)), ...
                               threeItemGARPTasks1323(2,2,threeItemGARPOrder1323(threeItemGARPIndex1323))];
     end
     
     
         if behavioral.key(i) == 'f';
-            reward.Item   = left.Item;jj
+            reward.Item   = left.Item;
             reward.Amount = left.Amount;
         else
             reward.Item   = right.Item;
