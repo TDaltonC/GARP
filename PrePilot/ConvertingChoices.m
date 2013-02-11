859048eed74286a2f69ebdd126befdf18d280c8c
Niree Kodaverdian <kodaverd@usc.edu>
2013-02-11 13:00:38 -0800

    Converting Tasks

--- PrePilot/ConvertingChoices.m
+++ PrePilot/ConvertingChoices.m
@@ -1,7 +1,7 @@
 %For converting representations of choices (1 and 2) to actual amounts of
 %each item selected 
 
-cd('/Users/Niree/Documents/MATLAB/GARP/preProcessed/Subjects/215');
+cd('/Users/Niree/Documents/MATLAB/GARP/preProcessed/214');
 % subjects = dir;
 % for folder = 1:length(subjects)
 %     if subjects(folder).name(1) == '.';
