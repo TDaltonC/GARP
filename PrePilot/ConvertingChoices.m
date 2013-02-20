5d12393a2aa3a74d8d4d075408b5663dde053daf
Niree Kodaverdian <kodaverd@usc.edu>
2013-02-19 22:07:00 -0800

    converts tasks for limit GARP

--- PrePilot/ConvertingChoices.m
+++ PrePilot/ConvertingChoices.m
@@ -1,13 +1,13 @@
 %For converting representations of choices (1 and 2) to actual amounts of
 %each item selected 
 
-cd('/Users/Niree/Documents/GitHub/GARP/prePilot/preProcessedRev/218');
-% subjects = dir;
-% for folder = 1:length(subjects)
-%     if subjects(folder).name(1) == '.';
-%         continue
-%     end
-%     cd(subjects(folder).name);
+cd('/Users/Niree/Documents/GitHub/GARP/prePilot/preProcessedRev/');
+subjects = dir;
+for folder = 1:length(subjects)
+    if subjects(folder).name(1) == '.';
+        continue
+    end
+    cd(subjects(folder).name);
     load('preProcessed.mat');
  
 % for i=1:length(preProcessed.limitGARP.tasks);
@@ -92,10 +92,20 @@ cd('/Users/Niree/Documents/GitHub/GARP/prePilot/preProcessedRev/218');
       preProcessed.threeItemGARP1323.revtasks(2,2,i) = preProcessed.threeItemGARP1323.tasks(2,1,i);
       preProcessed.threeItemGARP1323.revtasks(2,3,i) = preProcessed.threeItemGARP1323.tasks(2,2,i);
   end
-     
- 
-                  
+  
+  for i=1:length(preProcessed.limitGARP.tasks);
+      preProcessed.limitGARP.revtasks(1,1,i) = preProcessed.limitGARP.tasks(i,1);
+      preProcessed.limitGARP.revtasks(1,2,i) = 0;
+      preProcessed.limitGARP.revtasks(2,1,i) = 0;
+      preProcessed.limitGARP.revtasks(2,2,i) = preProcessed.limitGARP.tasks(i,2);
+  end
+  
 save('preProcessed.mat','preProcessed');
 
+cd('../');
+clearvars -except 'subjects', 'folder';
+
+
+end
 
  
