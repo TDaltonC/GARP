0ca29bd9f677e0469b581b9c24619eb42582c3dd
Niree Kodaverdian <kodaverd@usc.edu>
2013-08-08 20:45:53 -0700

    Response time

---
+++ ResponseTime.m
@@ -0,0 +1,32 @@
+
+% starts off in GARP.YAOABehaviorVersion.records
+cd('C:/Users/Niree/Documents/GitHub/GARP/PrePilot/records');
+subjects = dir;
+
+% goes into subject folders sequentially - starting at the top and going
+% proceeding one at a time - and creates a second column in behavioral.secs
+% for the difference in response time secs between each response. 
+
+load('1_20130124T120921.mat');
+for j = 1:(length(behavioral.secs(:,1)) - 1)
+    behavioral.secs(1,2) = behavioral.secs(1,1);
+    behavioral.secs(j+1,2) = behavioral.secs(j+1,1) - behavioral.secs(j,1);
+end
+save('1_20130124T120921.mat', 'behavioral');
+
+load('2_20130124T121034.mat');
+for j = 1:(length(behavioral.secs(:,1)) - 1)
+    behavioral.secs(1,2) = behavioral.secs(1,1);
+    behavioral.secs(j+1,2) = behavioral.secs(j+1,1) - behavioral.secs(j,1);
+end
+save('2_20130124T121034.mat', 'behavioral');
+    
+for i = 3:length(subjects)
+    load(subjects(i).name)
+    for j = 1:(length(behavioral.secs(:,1)) - 1)
+        behavioral.secs(1,2) = behavioral.secs(1,1);
+        behavioral.secs(j+1,2) = behavioral.secs(j+1,1) - behavioral.secs(j,1);
+    end
+    save(subjects(i).name, 'behavioral');
+end
+
