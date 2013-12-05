bf0c1b5f02e15ac8c1757fd57f9c8d4ee2968858
Dalton <tdaltoncombs@gmail.com>
2013-12-05 10:57:11 -0800

    Radicle House Keeping

--- transclosure.m
+++
@@ -1,8 +0,0 @@
-function [ transclosureB ] = transclosure( B )
-%UNTITLED5 Summary of this function goes here
-%   Detailed explanation goes here 
-
-transclosureB=sign(mpower(B,(length(B))));
-end
-
-
--- workspace/untitled.m
+++
@@ -1,8 +0,0 @@
-[h, fhat, xgrid] = kde(difscores, 401);
- figure;
- hold on;
- plot(xgrid, fhat, 'linewidth', 2, 'color', 'black');
- plot(difscores, zeros(length(difscores),1), 'b+');
- xlabel('Difference Scores')
- ylabel('Density function')
- hold off;
